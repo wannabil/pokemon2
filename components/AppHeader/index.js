bb3351a9ca47ed9765234235ae2d128e3f345ce2
Wan Muhammad Nabil Bin Wan Ismail <wannabil@Wans-MacBook-Air.local>
2024-08-10 18:15:37 +0800

    AppHeader placeholder

---
+++ components/AppHeader/index.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import styles from './AppHeader.module.css';
+
+const AppHeader = () => {
+  return (
+    <header>
+      {/* 
+      <div className={styles.headerContainer}>
+        <h1>App Header</h1>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+      */}
+    </header>
+  );
+};
+
+// export default AppHeader;
